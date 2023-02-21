b51b23e6e32d29c4ee1b9fe2807613c5fdae70de
Joan <setoba1192@hotmail.com>
2023-02-21 13:10:10 -0500

    carga variable regex para validacion de password
    carga de variable regex para establecerla en una variable de sistema

--- src/main/java/com/test/application/user/registration/UserRegistrationApplication.java
+++ src/main/java/com/test/application/user/registration/UserRegistrationApplication.java
@@ -1,13 +1,27 @@
 package com.test.application.user.registration;
 
+import jakarta.annotation.PostConstruct;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class UserRegistrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserRegistrationApplication.class, args);
-	}
+    @Value("${validation.password.pattern}")
+    private String passwordRegexp;
 
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setProperty() {
+        System.setProperty("validation.password.pattern", passwordRegexp);
+    }
 }
