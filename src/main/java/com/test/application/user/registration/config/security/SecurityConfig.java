8c6e9ae68a006afb1c64cd75ac4f95d6ebcafd05
Joan <setoba1192@hotmail.com>
2023-02-21 13:12:57 -0500

    agrega configuracion de seguridad
    se define el bean passwordEncoder
    se define el securityfilterchain

---
+++ src/main/java/com/test/application/user/registration/config/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.test.application.user.registration.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf().disable()
+                .authorizeHttpRequests(authorized -> authorized.requestMatchers("/api/usuario/**").permitAll()).build();
+    }
+}
