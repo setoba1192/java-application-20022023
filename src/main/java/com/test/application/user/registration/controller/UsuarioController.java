a83b60a5807dccc3938cc4cf91954b8e4fb091c2
Joan <setoba1192@hotmail.com>
2023-02-22 00:36:17 -0500

    refactor de método
    Ya no es necesario solicitar el header de autorizacion

--- src/main/java/com/test/application/user/registration/controller/UsuarioController.java
+++ src/main/java/com/test/application/user/registration/controller/UsuarioController.java
@@ -1,15 +1,10 @@
 package com.test.application.user.registration.controller;
 
 import com.test.application.user.registration.dto.RespuestaGenericaDTO;
-import com.test.application.user.registration.dto.UsuarioDTO;
 import com.test.application.user.registration.service.UsuarioService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/api/usuario")
@@ -19,7 +14,7 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<RespuestaGenericaDTO> consultarUsuario(@PathVariable String id, @RequestHeader(name = "Authorization") String token) {
+    public ResponseEntity<RespuestaGenericaDTO> consultarUsuario(@PathVariable String id) {
         return ResponseEntity.ok(RespuestaGenericaDTO.builder()
                 .mensaje("Consulta de usuario")
                 .data(this.usuarioService.findById(id))
