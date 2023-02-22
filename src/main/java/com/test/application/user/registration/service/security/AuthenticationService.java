cb46b2242c7b9792b10fe18c257c4251c8b1861b
Joan <setoba1192@hotmail.com>
2023-02-22 01:31:31 -0500

    refactor authenticationService

--- src/main/java/com/test/application/user/registration/service/security/AuthenticationService.java
+++ src/main/java/com/test/application/user/registration/service/security/AuthenticationService.java
@@ -1,76 +1,11 @@
 package com.test.application.user.registration.service.security;
 
-import com.test.application.user.registration.config.security.JwtService;
 import com.test.application.user.registration.dto.UsuarioDTO;
 import com.test.application.user.registration.dto.security.AuthenticationRequest;
-import com.test.application.user.registration.entity.Usuario;
-import com.test.application.user.registration.entity.security.Role;
-import com.test.application.user.registration.service.UsuarioService;
-import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-import java.util.Date;
+public interface AuthenticationService {
 
-@Service
-@RequiredArgsConstructor
-public class AuthenticationService {
+    UsuarioDTO registrarUsuario(UsuarioDTO usuarioDTO);
 
-    private final JwtService jwtService;
-
-    private final UsuarioService usuarioService;
-
-    private final UsuarioSecurityService usuarioSecurityService;
-
-    private final PasswordEncoder passwordEncoder;
-
-    private final AuthenticationManager authenticationManager;
-
-    private final ModelMapper modelMapper;
-
-    public UsuarioDTO registrarUsuario(UsuarioDTO usuarioDTO) {
-
-        var usuario = Usuario.builder()
-                .nombre(usuarioDTO.getNombre())
-                .correo(usuarioDTO.getCorreo())
-                .password(passwordEncoder.encode(usuarioDTO.getPassword()))
-                .role(Role.USER)
-                .build();
-
-        var jwtToken = jwtService.generateToken(usuario);
-        usuarioDTO.setLastLogin(new Date(System.currentTimeMillis()));
-        usuarioDTO.setToken(jwtToken);
-        UsuarioDTO usuarioRegistrado = usuarioService.crearUsuario(usuarioDTO);
-
-        return usuarioRegistrado;
-    }
-
-    /**
-     * Método para realizar login.
-     * Se establece el nuevo token para el usuario para que pueda persistir
-     *
-     * @param request
-     * @return
-     */
-    public UsuarioDTO login(AuthenticationRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getCorreo(), request.getPassword())
-        );
-
-        var usuario = usuarioSecurityService.findByCorreoForSecurity(request.getCorreo());
-
-        String token = jwtService.generateToken(usuario);
-
-        usuario.setToken(token);
-        usuario = this.usuarioSecurityService.updateLastLogin(usuario);
-
-
-        UsuarioDTO usuarioDTO = this.modelMapper.map(usuario, UsuarioDTO.class);
-        usuarioDTO.setPassword(null);
-
-        return usuarioDTO;
-    }
+    UsuarioDTO login(AuthenticationRequest request);
 }
