b1bc1672f76e454cec27e43de576a1b7c2acb9ab
Joan <setoba1192@hotmail.com>
2023-02-21 19:14:06 -0500

    implementacion de seguridad y login

---
+++ src/main/java/com/test/application/user/registration/dto/security/AuthenticationRequest.java
@@ -0,0 +1,17 @@
+package com.test.application.user.registration.dto.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    private String correo;
+
+    private String password;
+}
